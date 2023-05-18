77bf66fdce0174ab5dd42fb45b9bb045c11afd9a
sebastian medina <sebastianlopez_21@hotmail.com>
2023-05-18 11:22:04 -0400

    Seeds database

---
+++ app/controllers/owners_controller.rb
@@ -0,0 +1,7 @@
+class OwnersController < ApplicationController
+    
+    def index
+        owners = Owner.all
+        render json: owners
+      end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Owner.create(name: "Juan Medina", age: "28", image_url:"https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?cs=srgb&dl=pexels-mohamed-abdelghaffar-771742.jpg&fm=jpg")
+Owner.create(name: "Adriana Canizares", age: "28", image_url: "https://wallpapers.com/images/hd/cute-profile-picture-jeik5d6qizh6dxxr.jpg")
+Owner.create(name: "Dylan Hernandez", age: "21", image_url: "https://images.unsplash.com/photo-1628563694622-5a76957fd09c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW5zdGFncmFtJTIwcHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80")
